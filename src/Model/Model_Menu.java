768ef42d8e0722c5321b10721ae9b73f2a2b2194
user <Callmesammy@yahoo.com>
2022-08-25 22:19:30 +0100

    Slider

---
+++ src/Model/Model_Menu.java
@@ -0,0 +1,62 @@
+
+package Model;
+
+
+public class Model_Menu {
+
+    /**
+     * @return the icon
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * @param icon the icon to set
+     */
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Model_Menu(String icon, String name, String description) {
+        this.icon = icon;
+        this.name = name;
+        this.description = description;
+    }
+    
+    private String icon;
+    private String name;
+    private String description;
+
+    public Model_Menu() {
+    }
+    public void 
+}
